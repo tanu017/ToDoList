068bda1f4fea3bd35ed464a5f76fab76bd02fc64
tanu017 <tanuiyengar2005@gmail.com>
2025-06-02 18:12:42 +0530

    Update index.html

--- script.js
+++ script.js
@@ -5,6 +5,27 @@ function addTask() {
     let li = document.createElement("li");
     li.textContent = input;
     
+    let deleteBtn = document.createElement("img");
+    deleteBtn.src = "https://st2.depositphotos.com/1041273/5828/v/950/depositphotos_58283431-stock-illustration-trash-can-icon.jpg"; // Replace with your image URL
+    deleteBtn.alt = "Delete";
+    deleteBtn.style.width = "21px"; 
+    deleteBtn.style.cursor = "pointer";
+
+    deleteBtn.onclick = function() {
+        li.remove();
+    };
+
+    li.style.display = "flex";
+    li.style.justifyContent = "space-between";
+    li.style.alignItems = "center";
+    li.style.padding = "12px";
+    deleteBtn.style.color = "white";
+    deleteBtn.style.backgroundColor = "red";
+    deleteBtn.style.border = "2px solid black";
+    deleteBtn.style.borderRadius = "10px";
+    deleteBtn.style.fontSize = "20px";
+
+    li.appendChild(deleteBtn);
     document.getElementById("taskList").appendChild(li);
     document.getElementById("taskInput").value = ""; 
 }
